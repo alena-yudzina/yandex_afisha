21ddd99c7f0dece8c3d8bfff48177706dd1a8cce
Елена Юдина <alenayudzina@iMac-Elena.local>
2021-09-03 13:53:23 +0300

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,10 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'where_to_go.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,7 +82,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': Path(BASE_DIR) / 'db.sqlite3',
     }
 }
 
@@ -121,8 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 
 def show_index(request):
-    template = loader.get_template('index.html')
-    context = {}
-    rendered_page = template.render(context, request)
-    return HttpResponse(rendered_page)
+    return render(request, 'index.html')
