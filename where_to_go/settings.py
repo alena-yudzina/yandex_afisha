89d22609659d0abb5a36d479da02858515f25661
Елена Юдина <alenayudzina@iMac-Elena.local>
2021-09-14 15:13:28 +0300

    add default value to SECRET_KEY

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -24,15 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = []
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+DEBUG = True
 
 
 # Application definition
