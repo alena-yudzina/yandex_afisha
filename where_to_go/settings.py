204c8ed6b07ddce197331e34f84fdd3875cf18e4
Елена Юдина <alenayudzina@iMac-Elena.local>
2021-09-14 13:54:44 +0300

    add README.md

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -27,9 +27,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 
 # Application definition
